990d444ae2225916b3c3bd262457dfaa5450e23c
Emily Tran <trane@oxy.edu>
2019-02-26 15:02:20 -0800

    Don't remember

--- Game.java
+++ Game.java
@@ -11,11 +11,15 @@ public class Game{
 	}
 
 	void start() {
-		String WELCOME_MSG = "Both players are here, let's start!";
+		String WELCOME_MSG = "Both players are here, let's start! Player 1's turn to add ships.";
 		System.out.println(WELCOME_MSG);
 		player1.out.println(WELCOME_MSG);
 		player2.out.println(WELCOME_MSG);
-
+		player1.addShip();
+		if(player1.addShip()){
+			player2.addShip();
+		}
+		
 	}
 
 }
